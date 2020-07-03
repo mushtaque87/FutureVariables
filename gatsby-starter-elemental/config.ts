c00ee652622deb2c4971b1ece7b5fb544f4995cb
Mushtaque Ahmed <mushtaque87@gmail.com>
2020-07-03 11:17:58 +0400

    Initial Commit.

---
+++ gatsby-starter-elemental/config.ts
@@ -0,0 +1,167 @@
+const siteMetadata = {
+    title: `Future Variables`,
+    siteUrl: `http://localhost`,
+    capitalizeTitleOnHome: false,
+    logo: `/images/logo.png`,
+    icon: `/images/icon.png`,
+    titleImage: `/images/wall.png`,
+    ogImage: `/images/wall.png`,
+    twoColumnWall: true,
+    cookiePolicy: true,
+    introTag: `DEEP | FUTURISTIC | INNOVATIVE`,
+    description: `We Define Uncertain Future`,
+    about:`We we great!!!`,
+    author: `@_akzhy`,
+    blogItemsPerPage: 10,
+    portfolioItemsPerPage: 10,
+    darkmode: true,
+    switchTheme: true,
+    navLinks: [
+        {
+            name: "HOME",
+            url: "/",
+        },
+        {
+            name: "ABOUT",
+            url: "/about",
+        },
+        {
+            name: "BLOG",
+            url: "/blog",
+        },
+        {
+            name: "PORTFOLIO",
+            url: "/portfolio",
+        },
+        {
+            name: "CONTACT",
+            url: "/contact",
+        },
+    ],
+    footerLinks: [
+        {
+            name: "PRIVACY POLICY",
+            url: "/privacy-policy",
+        },
+        {
+            name: "GitHub",
+            url: "https://github.com/akzhy/gatsby-starter-elemental",
+        },
+    ],
+    social: [
+        {
+            name: "Facebook",
+            icon: "/images/Facebook.svg",
+            url: "#",
+        },
+        {
+            name: "Twitter",
+            icon: "/images/Twitter.svg",
+            url: "#",
+        },
+        {
+            name: "Instagram",
+            icon: "/images/Instagram.svg",
+            url: "#",
+        },
+        {
+            name: "Youtube",
+            icon: "/images/Youtube.svg",
+            url: "#",
+        },
+    ],
+    contact: {
+        // leave empty ('') or false to hide form
+        api_url: "https://getform.io/f/f227a36e-096a-4c6a-9963-9f1918a85bb3",
+        description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet accumsan arcu. Proin ac consequat arcu.`,
+        mail: "mushtaque87@gmail.com",
+        phone: "+971-545-269-920",
+        address: "1234 \nLocation \nLocation",
+    },
+    disqus: "elemental-netlify-com",
+}
+
+const beforeContactFormSubmit = data => {
+    // Code 0 - success
+    // Code 1 - Name
+    // Code 2 - Email
+    // Code 3 - Message
+    // Code 4 - Other
+    const errors = []
+
+    if (data.name.trim().length < 2) {
+        errors.push({
+            code: 1,
+            message: "Enter a name",
+        })
+    }
+
+    if (!data.email.match(/[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+/)) {
+        errors.push({
+            code: 2,
+            message: "Enter a valid email address",
+        })
+    }
+
+    if (data.message.trim().length < 15) {
+        errors.push({
+            code: 3,
+            message: "Enter a message with atleast 15 characters",
+        })
+    }
+
+    if (errors.length > 0)
+        return {
+            result: false,
+            errors: errors,
+        }
+
+    return {
+        data: {
+            name: data.name,
+            email: data.email,
+            message: data.message,
+        },
+        result: true,
+    }
+}
+
+const contactFormSubmit = async (api, data) => {
+    let res: any = await fetch(api, {
+        method: "POST",
+        body: JSON.stringify(data),
+        headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+        },
+    })
+
+    res = await res.json()
+
+    if (res.success) {
+        return {
+            result: true,
+        }
+    }
+    return {
+        result: false,
+        ...res,
+    }
+}
+
+const defaults = {
+    disqus: null,
+    twoColumnWall: true,
+    darkmode: false,
+    switchTheme: true,
+    capitalizeTitleOnHome: true,
+    cookiePolicy: false
+}
+
+Object.keys(defaults).forEach(item => {
+    if (siteMetadata[item] === undefined) {
+        siteMetadata[item] = defaults[item]
+    }
+})
+
+export { siteMetadata, beforeContactFormSubmit, contactFormSubmit }
